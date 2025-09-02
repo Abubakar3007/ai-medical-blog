6e249b462ae8a287d7a8c0eafcb6d54b069a5208
Abubakar Azmi <abubakarshaikh888@gmail.com>
2025-09-02 12:30:35 +0530

    change backend server.js

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require("path");
+
+// Serve frontend build
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Serve static images
 
 const PORT = process.env.PORT || 5000;
